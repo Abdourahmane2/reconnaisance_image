2c33dbad0b790c31ce468ad793853c3cce7931ec
abdourahmane <abdourahmanetimera@esp.sn>
2024-01-05 23:17:02 +0100

    make it better

--- nchien_chat/urls.py
+++
@@ -1,8 +0,0 @@
-# chien_chat/urls.py
-from django.urls import path
-from .views import home
-
-urlpatterns = [
-    path('home/', home, name='home'),
-    # Ajoutez d'autres chemins si nécessaire
-]
