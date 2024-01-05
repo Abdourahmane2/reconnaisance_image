c917075286aec473d37690ff83408bd9a4ff2ed4
abdourahmane <abdourahmanetimera@esp.sn>
2024-01-05 18:36:13 +0100

    suprresion

--- nchien_chat/views.py
+++ nchien_chat/views.py
@@ -1,3 +1,4 @@
+import base64
 from django.shortcuts import render
 from django.http import HttpResponse 
 import tensorflow as tf 
@@ -8,10 +9,13 @@ import io
 model = tf.keras.models.load_model('model_chein_chat')
 
 # chien_chat/views.py
+
+
 def home(request): 
     imagefile = request.FILES.get('imagefile')
     if imagefile is not None: 
         image_data = imagefile.read()
+        encoded_image = base64.b64encode(image_data).decode('utf-8')
         test_image = image.load_img(io.BytesIO(image_data), target_size=(128, 128))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
@@ -22,6 +26,6 @@ def home(request):
         else:
             predictions = "chien"
         
-        return render(request, 'result.html', {'predictions': predictions, 'image_data': image_data})
+        return render(request, 'result.html', {'predictions': predictions, 'image_data': encoded_image})
     else: 
         return render(request, 'index.html')
